c1721941b86c93c1339fb479e66aac488bebb239
Cuong Lam <quoccuong042009@yahoo.com>
2017-12-09 21:31:29 -0500

    update API
    sua ten dibe,
    xoa model dư

--- API/urls.py
+++ API/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework.urlpatterns import format_suffix_patterns
-from webapp import views
+from dibe import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
