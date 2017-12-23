39db11ca624c4d0077e0dc211bb11cdb19610c73
tnlong1997 <tnlong1997@gmail.com>
2017-12-23 22:50:27 +0700

    Test deploy

--- API/settings.py
+++ API/settings.py
@@ -111,8 +111,8 @@ if 'test' in sys.argv:
         }
     }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
