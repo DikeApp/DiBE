022222f7456ab5629a810828ab9563d8123ddb5a
tnlong1997 <tnlong1997@gmail.com>
2018-04-04 19:04:02 -0400

    Add mlab mongo db

--- node_server/app.js
+++ node_server/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -42,5 +43,13 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//Set up mongoose connection
+databaseUrl = 'mongodb://tnlong1997:bdragon7125..@ds233739.mlab.com:33739/dike_test';
+var mongoDB = databaseUrl;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 module.exports = app;
