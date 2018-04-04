b6704c9b59925c2f4fae2f90d2051da0b7102135
tnlong1997 <tnlong1997@gmail.com>
2018-04-04 19:31:52 -0400

    Add sign up to router

--- node_server/routes/users.js
+++ node_server/routes/users.js
@@ -5,4 +5,7 @@ var userController = require('../controllers/userController')
 /* GET users listing. */
 router.get('/', userController.user_list);
 
+/* POST sign up new user */
+router.post('/', userController.user_sign_up);
+
 module.exports = router;
