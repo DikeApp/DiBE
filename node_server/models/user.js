b33ea2254f996f5d0c70411650cafe0ac290509d
tnlong1997 <tnlong1997@gmail.com>
2018-04-05 00:13:41 -0400

    Set unique validator

--- node_server/models/user.js
+++ node_server/models/user.js
@@ -1,13 +1,27 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 
 var Schema = mongoose.Schema;
 
 userSchema = new Schema(
   {
-    username: {type: String, required: true, max: 100, min: 8},
-    password: {type: String, required: true, max: 50, min: 8},
+    username: {
+      type: String,
+      required: true,
+      maxLength: 100,
+      minLength: 8,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      maxLength: 50,
+      minLength: 8,
+    },
   }
 );
+
+userSchema.plugin(uniqueValidator);
 
 //Export model
 module.exports = mongoose.model('User', userSchema);
