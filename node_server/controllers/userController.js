37410fa3ab148d3f0995e8a773307d1bb8f955a0
tnlong1997 <tnlong1997@gmail.com>
2018-04-09 18:12:02 -0400

    Add login function in user controller

--- node_server/controllers/userController.js
+++ node_server/controllers/userController.js
@@ -1,5 +1,7 @@
 var User = require('../models/user');
 var bcrypt = require('bcrypt-nodejs');
+var secret = require('../config/secret');
+var jwt = require('jsonwebtoken');
 
 // Display all user
 exports.user_list = function(req, res, next) {
@@ -18,6 +20,7 @@ exports.user_list = function(req, res, next) {
   // res.send('User list');
 }
 
+// Sign up new user
 exports.user_sign_up = function(req, res, next) {
 
   var newUser = new User(
@@ -53,4 +56,61 @@ exports.user_sign_up = function(req, res, next) {
       });
     }
   });
+}
+
+// User login
+exports.user_log_in = function(req, res, next) {
+
+  if (!req.body.username) {
+    return res.send({
+      success: false,
+      code: 400,
+      err: "No email in input"
+    });
+  }
+
+  if (!req.body.password) {
+    return res.send({
+      success: false,
+      code: 400,
+      err: "No password in input"
+    })
+  }
+
+  User.findOne({username: req.body.username})
+    .exec(function(err, user) {
+      if (err) {
+        return res.send({
+          success: false,
+          code: 400,
+          err: err
+        });
+      }
+
+      if (!user) {
+        return res.send({
+          success: false,
+          code: 400,
+          err: "Authenticate failed. User not found"
+        });
+      }
+
+      user.comparePassword(req.body.password, function(err, isMatch) {
+        if (isMatch && !err) {
+          // Create token if the password matched and no error was thrown
+          var token = jwt.sign({username: user.username, _id: user._id}, secret, {
+            expiresIn: 1000000000000000 // in seconds
+          });
+          res.json({
+            success: true,
+            token: 'JWT ' + token,
+          });
+        } else {
+          res.send({
+            success: false,
+            message: 'Authentication failed. Passwords did not match.'
+          });
+        }
+      })
+    });
 }
