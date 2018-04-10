1b66b07332951b4409fc20ee7021c83fceedf9c8
tnlong1997 <tnlong1997@gmail.com>
2018-04-09 23:39:32 -0400

    set app secret

--- node_server/app.js
+++ node_server/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var secret = require('config/secret');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -14,6 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '-i');
+app.set('secret', secret);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
