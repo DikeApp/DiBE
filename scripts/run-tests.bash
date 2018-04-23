953daa03897c324e9e5f8df79818da9426dd683a
tnlong1997 <tnlong1997@gmail.com>
2018-04-22 23:10:25 -0400

    Dm quen dang lam gi roi

--- scripts/run-tests.bash
+++ scripts/run-tests.bash
@@ -11,11 +11,10 @@ cd "${0%/*}/.."
 # let's fake failing test for now
 echo "Running tests"
 echo "............................"
-echo "Failed!" && exit 1
 
 # example of commands for different languages
 # eslint .         # JS code quality check
 # npm test         # JS unit tests
 # flake8 .         # python code quality check
 # nosetests        # python nose
-# just put your usual test command here 
+# just put your usual test command here
